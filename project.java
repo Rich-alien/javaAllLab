5f4cf1885ac60fa8caf9ea0ce6c2ea705bd59c63
Rich <Richalien@yandex.ru>
2020-02-19 15:18:56 +0400

    Проверка теории

--- project.java
+++ project.java
@@ -1,7 +1,15 @@
+import java.nio.ByteBuffer;
 class Simple{
 public static void main(String[] args) {
-    int[] someArr = new int[20];
- 
+//    Создали массив типа Byte с K эллементами 
+    int K=20;
+    byte[] ArrByte = new byte[K];
+    Integer[] ArrInt = new Integer[ArrByte.length];
+    Byte[] ArrByteBig = new Byte[K];
+    //
+    for (int i = 0; i < ArrInt.length; i++) {
+    ArrByteBig[i] = (Byte)ArrByte[i];         
+    }
 
 }
 }
