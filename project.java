6c47b8e41e1dd5714b06b2178325d576c1920b3a
Rich <Richalien@yandex.ru>
2020-02-19 15:32:09 +0400

    часть тестирования

--- project.java
+++ project.java
@@ -4,12 +4,34 @@ public static void main(String[] args) {
 //    Создали массив типа Byte с K эллементами 
     int K=20;
     byte[] ArrByte = new byte[K];
-    Integer[] ArrInt = new Integer[ArrByte.length];
     Byte[] ArrByteBig = new Byte[K];
+    Integer[] ArrInt = new Integer[ArrByte.length];
+    int[] ArrIntMin = new int[ArrByte.length];
+    String[] ArrString = new String[K];
+   
     //
-    for (int i = 0; i < ArrInt.length; i++) {
+      for (int i = 0; i < ArrInt.length; i++) {
     ArrByteBig[i] = (Byte)ArrByte[i];         
     }
+
+
+    // проверка теории
+      for (int i = 0; i < ArrInt.length; i++) {
+        ArrIntMin[i] = (int)ArrByteBig[i];         
+    }
+    //test test
+
+
+
+      for (int i = 0; i < ArrInt.length; i++) {
+    ArrInt[i] = (Integer)ArrIntMin[i];         
+    }
+
+    for (int i = 0; i < ArrInt.length; i++) {
+       // ArrString[i] = (String)ArrInt[i];         
+    }
+      
+
 
 }
 }
